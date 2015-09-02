7cbd1bc7c43b29f628aa3d9f472dd6416cb00eb0
frhd <farhadino@gmail.com>
2015-09-02 23:57:34 +0200

    add iss icon, change os x bright/dark theme on click

--- iss-tracker/iss-tracker/AppDelegate.m
+++ iss-tracker/iss-tracker/AppDelegate.m
@@ -11,16 +11,96 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
+@property (strong, nonatomic) NSStatusItem *statusItem;
+@property (assign, nonatomic) BOOL darkModeOn;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    _statusItem.image = [NSImage imageNamed:@"iss.png"];
+    [_statusItem.image setTemplate:YES];
+    
+    _statusItem.highlightMode = NO;
+    _statusItem.toolTip = @"control-click to quit";
+    
+    [_statusItem setAction:@selector(itemClicked:)];
+    [self refreshDarkMode];
+}
+
+- (void)refreshDarkMode {
+    NSString * value = (__bridge NSString *)(CFPreferencesCopyValue((CFStringRef)@"AppleInterfaceStyle", kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost));
+    if ([value isEqualToString:@"Dark"]) {
+        self.darkModeOn = YES;
+    }
+    else {
+        self.darkModeOn = NO;
+    }
+}
+
+- (void)itemClicked:(id)sender {
+    
+    NSEvent *event = [NSApp currentEvent];
+    if([event modifierFlags] & NSControlKeyMask) {
+        [[NSApplication sharedApplication] terminate:self];
+        return;
+    }
+    
+    _darkModeOn = !_darkModeOn;
+    
+    //Change pref
+    if (_darkModeOn) {
+        [self makeItDark];
+    }
+    else {
+        [self makeItBright];
+    }
+}
+
+- (void)makeItDark {
+    //Set theme and update listeners
+    CFPreferencesSetValue((CFStringRef)@"AppleInterfaceStyle", @"Dark", kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        CFNotificationCenterPostNotification(CFNotificationCenterGetDistributedCenter(), (CFStringRef)@"AppleInterfaceThemeChangedNotification", NULL, NULL, YES);
+    });
+}
+
+- (void)makeItBright {
+    //Set theme and update listeners
+    CFPreferencesSetValue((CFStringRef)@"AppleInterfaceStyle", NULL, kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        CFNotificationCenterPostNotification(CFNotificationCenterGetDistributedCenter(), (CFStringRef)@"AppleInterfaceThemeChangedNotification", NULL, NULL, YES);
+    });
+}
+
+- (void)changeWallpaperWithImagePath:(NSString *)path {
+    
+    //If NinjaModes directory does not exist, assume not interested.
+    if (![[NSFileManager defaultManager] fileExistsAtPath:[@"~/Documents/NinjaModes" stringByExpandingTildeInPath]]) {
+        return;
+    }
+    
+    NSError *error;
+    [[NSWorkspace sharedWorkspace] setDesktopImageURL:[NSURL fileURLWithPath:path]
+                                            forScreen:[NSScreen mainScreen]
+                                              options:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                       nil, NSWorkspaceDesktopImageFillColorKey,
+                                                       [NSNumber numberWithBool:NO], NSWorkspaceDesktopImageAllowClippingKey,
+                                                       [NSNumber numberWithInteger:NSImageScaleProportionallyUpOrDown], NSWorkspaceDesktopImageScalingKey, nil]
+                                                error:&error];
+    if (error) {
+        [[NSApplication sharedApplication] presentError: error
+                                         modalForWindow: self.window
+                                               delegate: nil
+                                     didPresentSelector: nil
+                                            contextInfo: NULL];
+    }
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
-}
-
+    //Attempt to restore things back the way we found them
+    [self makeItBright];}
 @end
